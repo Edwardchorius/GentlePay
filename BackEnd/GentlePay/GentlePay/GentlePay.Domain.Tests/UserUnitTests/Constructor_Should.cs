e28cb04e356dfad189a4c0c8e5de3eadc4d5d04d
Edwardchorius <edwardcho96@abv.bg>
2020-01-22 01:19:54 +0200

    Added unit tests for User entity, added constructor and logic for user instance initialization

--- BackEnd/GentlePay/GentlePay/GentlePay.Domain.Tests/UserUnitTests/Constructor_Should.cs
+++ BackEnd/GentlePay/GentlePay/GentlePay.Domain.Tests/UserUnitTests/Constructor_Should.cs
@@ -1,8 +1,6 @@
 ﻿using GentlePay.Domain.Entities;
 using NUnit.Framework;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace GentlePay.Domain.Tests.UserUnitTests
 {
@@ -22,6 +20,39 @@ namespace GentlePay.Domain.Tests.UserUnitTests
             //Assert
             Assert.IsNotNull(newUser);
             Assert.IsInstanceOf(typeof(User), newUser);
+            Assert.IsNotNull(newUser.FirstName);
+            Assert.IsNotNull(newUser.LastName);
+            Assert.IsNotNull(newUser.Accounts);
+            Assert.AreEqual(newUser.FirstName, firstName);
+            Assert.AreEqual(newUser.LastName, lastName);
+        }
+
+        [TestCase(" ")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Throw_ArgumentException_When_FirstName_IsNullOrWhiteSpace(string inputValueForFirstName)
+        {
+            //Arrange
+            string lastName = "lastName";
+
+            User newUser;
+
+            //Act && Assert
+            Assert.Throws<ArgumentException>(() => newUser = new User(inputValueForFirstName, lastName));
+        }
+
+        [TestCase(" ")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Throw_ArgumentException_When_LastName_IsNullOrWhiteSpace(string inputValueForLastName)
+        {
+            //Arrange
+            string firstName = "lastName";
+
+            User newUser;
+
+            //Act && Assert
+            Assert.Throws<ArgumentException>(() => newUser = new User(firstName, inputValueForLastName));
         }
     }
 }
--- BackEnd/GentlePay/GentlePay/GentlePay.Domain/Entities/User.cs
+++ BackEnd/GentlePay/GentlePay/GentlePay.Domain/Entities/User.cs
@@ -8,7 +8,21 @@ namespace GentlePay.Domain.Entities
     {
         public User(string firstName, string lastName)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentException(firstName);
+            }
 
+            this.FirstName = firstName;
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentException(lastName);
+            }
+
+            this.LastName = lastName;
+
+            this.Accounts = new List<Account>();
         }
 
         public string FirstName { get; set; }
